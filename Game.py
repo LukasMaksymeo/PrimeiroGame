aeb93cab439d04a71764705d556bb6ef8d556301
root <root@DESKTOP-VAV0F8S>
2024-02-06 14:42:30 -0300

    teste

--- Game.py
+++ Game.py
@@ -1,1 +1,32 @@
-codigo
+classe=("Barbaro","Paladino","Assasino")
+print("      Olá Jogador!!!      \n"
+      "    Escolha sua classe    \n"
+      "", classe, "")
+opcao = (input())
+if (opcao == "barbaro"):
+    print("teste barbaro funcionou")
+    vida = 30
+    ataque = 5
+elif (opcao == "paladino"):
+    print("teste paladino funcionou")
+    vida = 50
+    ataque = 3
+elif (opcao == "assasino"):
+    print("teste assasino funcionou")
+    vida = 20
+    ataque = 6
+else:
+    print("erro")    
+print("hora da batalha ")
+inimigo = "goblin"
+vida_Inimigo = 13
+atq_Inimigo = 2 
+while(vida > 0 and vida_Inimigo > 0): ### fase de batalha, transformar em uma função ###
+    print(inimigo, "atacou você")
+    vida = vida - atq_Inimigo
+    print("você ataca")
+    vida_Inimigo = vida_Inimigo - ataque
+    if (vida == 0):
+        print("você morreu!")
+    else:
+        print("você ganhou")
