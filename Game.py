284878a91d42e651ff60e0d1d1aa9913f39a1ec7
LukasMaksymeo <lukas_roveri@hotmail.com>
2024-02-09 19:03:34 -0300

    concerto de bug

--- Game.py
+++ Game.py
@@ -18,7 +18,7 @@ elif (opcao == "paladino"):
     Atq_Crit = 0    
 elif (opcao == "assasino"):
     print("teste assasino funcionou")
-    vida = 200
+    vida = 20
     ataque = 6
     Crit_chance = [1,2,3]
     Atq_Crit = 0
@@ -26,15 +26,15 @@ else:
     print("erro")    
 print("hora da batalha ")
 inimigo = "goblin"
-vida_Inimigo = 150
-atq_Inimigo = 4 
+vida_Inimigo = 30
+atq_Inimigo = 4
 while(vida > 0 and vida_Inimigo > 0): ### fase de batalha, transformar em uma função ###
     print(inimigo, "atacou você")
     vida = vida - atq_Inimigo
     print("você ataca")
     Atq_Crit = Crit.Critico(Crit_chance, ataque)
     vida_Inimigo = vida_Inimigo - (ataque + Atq_Crit)
-    if (vida == 0):
+    if (vida <= 0):
         print("você morreu!")
-    elif (vida_Inimigo == 0):
+    elif (vida_Inimigo <= 0):
         print("você ganhou")
