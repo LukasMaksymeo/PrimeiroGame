19e762d618a2746f313b72920012cf22f7c23836
LukasMaksymeo <lukas_roveri@hotmail.com>
2024-02-20 20:08:09 -0300

    att nos teste

--- testes.py
+++ testes.py
@@ -1,4 +1,4 @@
-import Crit
+import Crit   ###import de um conteudo dentro de outro arquivo na mesma pasta
 vida = 20                   #      vez do jogador
 ataque = 2                  #
 Crit_chance = [1,2]       #
@@ -13,6 +13,13 @@ while(vida > 0 and vida_Inimigo > 0): ### fase de batalha, transformar em uma fu
     Atq_Crit = Crit.Critico(Crit_chance, ataque)
     vida_Inimigo = vida_Inimigo - (ataque + Atq_Crit)
     print(Atq_Crit, ataque)
-###-------------------------------------------------------------------------------------
+###----------------------------------------------------------------------------
 import os           ### comando para limpar o terminal, caso precise
 os.system('clear') or None
+###----------------------------------------------------------------------------
+### Comando para fazer lowercase e o cancelar os espaçamentos 
+###----------------------------------------------------------------------------
+from tkinter import* ### Doc que Traz as telas
+janela = Tk()               ### não está funcionando 
+janela.mainloop()
+###----------------------------------------------------------------------------
