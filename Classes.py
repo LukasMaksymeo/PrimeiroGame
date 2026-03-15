1b26bdbd06fd133423b74e2f48db29b82bdbd54b
Lukas Roveri Maksymeo <lukas_roveri@hotmail.com>
2026-03-14 22:11:29 -0300

    Inserção de Classes de personagem em formato POO

--- Classes.py
+++ Classes.py
@@ -24,7 +24,33 @@ from pygame.examples.midi import NullKey
 #         Crit_chance = [1,2,3]
 #         Atq_Crit = 0
 #         return vida, ataque, Crit_chance, Atq_Crit
-class Barbaro ():
+
+class Personagem:
+    def __init__(self, vida=0, ataque=0,critico=0,bloqueio=0):
+        self.vida = vida
+        self.ataque = ataque
+        self.critico = critico
+        self.bloqueio =bloqueio
+
+    def sofrer_dano(self, dano):
+        self.vida -= dano
+        print(self.vida)
+    def realizar_ataque(self):
+        return self.ataque
+
+class Barbaro(Personagem):
     def __init__(self):
-        vida = 35
-        ataque = 7
+        super().__init__(vida=35, ataque=7)
+
+class Assassino(Personagem):
+    def __init__(self):
+        super().__init__(vida=48, ataque=3, critico=5)
+
+class Paladino(Personagem):
+    def __init__(self):
+        super().__init__(vida=27, ataque=3,critico=2,bloqueio=3)
+
+
+print("Insira seu dano")
+lukas = Assassino()
+lukas.sofrer_dano(int(input()))
